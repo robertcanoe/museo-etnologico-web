d012a1c643149a492968a131f24c6a1fe2b9aa39
robertcano <rcanoestevez@proton.me>
2025-08-14 12:10:53 +0200

    Replace exhibitions component with reviews in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
 import { About } from "@/components/about"
 import { Gallery } from "@/components/gallery"
-import { Exhibitions } from "@/components/exhibitions"
+import { Reviews } from "@/components/reviews"
 import { Contact } from "@/components/contact"
 import { Footer } from "@/components/footer"
 
@@ -13,7 +13,7 @@ export default function HomePage() {
       <Hero />
       <About />
       <Gallery />
-      <Exhibitions />
+      <Reviews />
       <Contact />
       <Footer />
     </main>
